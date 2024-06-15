95bbdf29fb65d0c34fb562a436e65c2fb638750d
ShiftyX1 <143071695+ShiftyX1@users.noreply.github.com>
2024-06-15 22:19:48 +0300

    update script

--- prepare_environment.ps1
+++ prepare_environment.ps1
@@ -1,21 +1,15 @@
 # Определение путей к подкаталогам
 $envPaths = @("api", "bot", "front")
 
-# Создание виртуальных окружений и установка модулей
 foreach ($envPath in $envPaths) {
-    # Проверка наличия подкаталога, создание если отсутствует
     if (-Not (Test-Path $envPath)) {
-        # New-Item -ItemType Directory -Path $envPath
         continue
     }
 
-    # Создание виртуального окружения
     python -m venv "$envPath\venv_$envPath"
 
-    # Активация виртуального окружения
     & "$envPath\venv\Scripts\Activate.ps1"
 
-    # Установка модулей из requirements.txt, если файл существует
     $requirementsPath = "$envPath\requirements.txt"
     if (Test-Path $requirementsPath) {
         pip install -r $requirementsPath
@@ -23,6 +17,5 @@ foreach ($envPath in $envPaths) {
         Write-Host "requirements.txt не найден в $envPath"
     }
 
-    # Деактивация виртуального окружения
     deactivate
 }
